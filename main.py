ae0375df5b955bab96d26af3ab96ef09f808a010
jmthier <jmthier@gmail.com>
2023-03-23 14:33:29 -0700

    Clean main()

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Testing/Using helper functions
+import Helpers
+from Helpers import *
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+print("3... 2... 1... GO!\n")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+DBG("Debugging not enabled.")
+Console("Enabling Debugging now...")
+Helpers.PrintDebugMessages = True
+DBG("Debugging Enabled.")
+
+print("\nYeah, feeling good about that! :) ")
