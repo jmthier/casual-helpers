7ed501a2c4f3bb8a9d6d003cb3f641e2e74b60b4
jmthier <jmthier@gmail.com>
2023-12-06 18:32:45 -0800

    moved to object oriented!
    added IO_Handler class
    added CLI_Handler class
    added text formatting class (aka, enum)
    changed main() to properly test IO_Handler class.

--- Helpers.py
+++ Helpers.py
@@ -3,82 +3,180 @@
 # Error Messaging
 # Print functions for better visualization.
 
-import datetime as datetime
+from datetime import datetime
 import os
+import argparse
 
-# "Global" variables stored here
-DateTimeStart = datetime.datetime.now()
-DebugLogFName = ""
-DataOutFName = ""
-ConsoleOutFName = ""
-DebugLogDirectory = os.getcwd() + "\\"
-DataOutDirectory = os.getcwd() + "\\"
-ConsoleOutDirectory = os.getcwd() + "\\"
+class bTextOptions:
+    """constants for print() colors and styles"""
 
-# User Settings
-LogDebugMessages = False
-PrintDebugMessages = False
-LogConsoleOutput = False
-PrintConsoleOutput = True
+    class Color:
+        """basic colors"""
+        BASIC = '\033[0m'
+        PURPLE = '\033[95m'
+        BLUE = '\033[94m'
+        CYAN = '\033[96m'
+        YELLOW = '\033[093m'
+        RED = '\033[91m'
+        GREEN = '\033[92m'
+
+    class Style:
+        """basic styles"""
+        BOLD = '\033[1m'
+        UNDERLINE = '\033[4m'
+        DIM = '\033[2m'
+        ITALIC = '\033[3m'
+
+class CommandLine_Handler:
+    """Class for Command Line Argument Handling"""
+
+    io = None
+    args = None
+
+    def __init__(self, io):
+        parser = argparse.ArgumentParser(prog="Bonfire Fodder",
+                                         description="Interacting with Bonfire development boards",
+                                         epilog="Comments?? haha")
+        group = parser.add_mutually_exclusive_group()
+        group.add_argument("-v", "--verbosity",
+                            help="1, 2, or 3 levels of output verbosity.  [-v][-vv][-vvv]",
+                            action="count",
+                            default=0)
+        group.add_argument("-s", "--silent",
+                            help="no output at all",
+                            action="store_true",)
+        parser.add_argument("-l", "--log",
+                            help="If used, output will be logged to a text file in the current working directory",
+                            action="store_true")
+        parser.add_argument("-a", "--app",
+                            help="Run the shell app, user can try and try again.",
+                            action="store_true")
+        parser.add_argument("-r", "--run",
+                            help="Runs a specific app specified after the argument",
+                            default="")
+        parser.add_argument("-f", "--finddevices",
+                            help="This will scan the PC for any connected Bonfire devices",
+                            action="store_true")
+
+        self.io = io
+        self.args = parser.parse_args()
+        self.io.verbosity = self.args.verbosity
+        self.io.silent = self.args.silent
+
+    def parse_args(self):
+        # Decide what to do based on command line args
+
+        if self.args.log:
+            self.io.LogOutput = True
+            # self.io.LogFName = "DebugLog_" + DateTimeStart.strftime("%Y%m%d_%H%M") + ".txt"
+            self.io.out("-l/-log flag received.", 1)
+
+        if self.args.finddevices:
+            self.io.out("-f/--finddevices flag received", 1)
+
+        if self.args.app:
+            self.io.out("-a/--app flag received", 1)
+
+        if self.args.run:
+            self.io.out("-r/--run flag received", 1)
+
+        return self.args
 
 
-def ExitError(mess):
-    print("ERROR::" + mess)
-    exit()
-    return 0
+class IO_Handler:
+    """Class for better IO for files, visuals formatting, etc."""
 
+    silent = False
+    verbosity = 0
+    LogOutput = False
+    LogFName = ""
+    DataOutFName = ""
+    LogDirectory = os.getcwd() + "\\"
+    DataOutDirectory = os.getcwd() + "\\"
 
-# prints a message that can be overwritten unless next print() begins with \n
-def PrintOver(mess, end=False):
-    global PrintConsoleOutput
+    @property
+    def start_date_time(self):
+        """Returns the date and time the value was initialized (should be at program start)"""
+        return self._start_date_time
 
-    if PrintConsoleOutput:
+    def __init__(self):
+        self.log_directory = os.getcwd() + "\\"
+        self.data_out_directory = os.getcwd() + "\\"
+        self._start_date_time = datetime.now()
+
+    def out(self, mess, v_level, dbg=False, style=None, overwrite=None):
+        """overloading print().  outputs [mess] when verbosity >= v_level"""
+        # Suggested v_level uses:
+        #  0 - status messages (e.g., "scanning devices" or "progress - 85%")
+        #  1 - program flow indications (e.g., "initializing X" or "creating output file OutFile.txt")
+        #  2 - detailed variable status (e.g., "running function X() with args (Y)(Z)")
+
+        if dbg:
+            mess = "[DBG] :: " + mess
+
+        match style:
+            case "RED":
+                style = bTextOptions.Color.RED
+            case "BLUE":
+                style = bTextOptions.Color.BLUE
+            case "CYAN":
+                style = bTextOptions.Color.CYAN
+            case "YELLOW":
+                style = bTextOptions.Color.YELLOW
+            case "GREEN":
+                style = bTextOptions.Color.GREEN
+            case "PURPLE":
+                style = bTextOptions.Color.PURPLE
+            case "BOLD":
+                style = bTextOptions.Style.BOLD
+            case "UNDERLINE":
+                style = bTextOptions.Style.UNDERLINE
+            case "DIM":
+                style = bTextOptions.Style.DIM
+            case "ITALIC":
+                style = bTextOptions.Style.ITALIC
+            case None:
+                style = bTextOptions.Color.BASIC
+            case _:
+                style = bTextOptions.Color.BASIC
+
+        timestamp_string = ""
+        if self.LogOutput:
+            now = datetime.now()
+            timestamp_string = "[" + now.strftime("%d/%m/%Y %H:%M:%S") + "] "
+            if self.LogFName == "":
+                print(f"{bTextOptions.Color.YELLOW}WARNING: No log filename specified, "
+                      f"defaulting to Log_<date_time>.txt{bTextOptions.Color.BASIC}")
+                self.LogFName = "Log_" + self.start_date_time.strftime("%Y%m%d_%H%M") + ".txt"
+
+        if not self.silent:
+            # silent flag not set in arguments
+            if self.verbosity >= v_level:
+                # User wants to see it
+                if overwrite is None:
+                    print(f"{style}{mess}{bTextOptions.Color.BASIC}", flush=True)
+                else:
+                    print(f"{style}{mess}{bTextOptions.Color.BASIC}", end="", flush=True)
+
+                if self.LogOutput:
+                    with open(self.LogFName, 'a') as f:
+                        f.write(timestamp_string + mess + "\n")
+
+        return
+
+    def exit_error(self, mess):
+        mess = "ERROR::" + mess
+        self.out(mess, v_level=0, style="RED")
+        exit()
+
+    # prints a message that can be overwritten unless next print() begins with \n
+    def over_write(self, mess, end=False):
+        """Function for Copying over the previous line of output"""
         line = "\r" + str(mess)
-        print(line, end="", flush=True)
+        self.out(line, 1, overwrite=True)
         if end:
             print("\n")
-    return 0
 
-
-# handling a debug print statement in logging or console according to vars
-def DBG(mess):
-    global DebugLogFName
-    mess = "[DBG] :: " + mess
-
-    if PrintDebugMessages:
-        print(mess)
-
-    if LogDebugMessages:
-        now = datetime.datetime.now()
-        TimeStamp_string = "[" + now.strftime("%d/%m/%Y %H:%M:%S") + "] "
-
-        if DebugLogFName == "":
-            print("No Debug Log File specified, defaulting to DebugLog_<date_time>.txt")
-            DebugLogFName = "DebugLog_" + DateTimeStart.strftime("%Y%m%d_%H%M") + ".txt"
-
-        with open(DebugLogFName, 'a') as f:
-            f.write(TimeStamp_string + mess + "\n")
-    return
-
-
-# Use this instead of print to handle if/where the output is shown
-def Console(mess):
-    global ConsoleOutFName
-
-    if PrintConsoleOutput:
-        print(mess)
-
-    if LogConsoleOutput:
-        now = datetime.datetime.now()
-        TimeStamp_string = "[" + now.strftime("%d/%m/%Y %H:%M:%S") + "] "
-
-        if ConsoleOutFName == "":
-            print("No Console Output File specified, defaulting to ConsoleOut_<date_time>.txt")
-            ConsoleOutFName = "ConsoleOut_" + DateTimeStart.strftime("%Y%m%d_%H%M") + ".txt"
-
-        with open(ConsoleOutFName, 'a') as f:
-            f.write(TimeStamp_string + mess + "\n")
-    return
-
+        return 0
 
 # EOF
--- main.py
+++ main.py
@@ -1,13 +1,63 @@
 # Testing/Using helper functions
-import Helpers
-from Helpers import *
+
+# Imports
+from Helpers import CommandLine_Handler, IO_Handler
+import time
+
+vLevels = {0, 1, 2}
+
+def main():
+    """main() for testing the Helpers() file."""
+
+    io = IO_Handler()
+
+    # check all verbosity levels
+    for vl in vLevels:
+        io.verbosity = vl
+        print(f"Verbosty set to {vl}")
+        io.out("L2 detail -> Start with a DBG statement", 2, dbg=True)
+        io.out("L2 detail -> but doesn't have to be...", 2)
+        io.out("L1 detail -> we started main()", 1)
+        io.out("L0 detail -> Beginning Test of Helpers.py", 0)
+        time.sleep(2)
+
+    print("Verbosity Test Complete")
+    time.sleep(3)
+
+    print("Verbosity back to max(2) for the rest of the tests.")
+    IO_Handler.verbosity = 2
+    print("Starting Countdown")
+    sec = 5
+    while sec > 0:
+        io.over_write(f"Countdown: {sec}", end=False)
+        time.sleep(1)
+        sec -= 1
+    io.over_write(f"Countdown: {sec}", end=True)
+
+    print("Format Check:")
+    io.out("This should be normal text", 0)
+    io.out("YELLOW", 0, style="YELLOW")
+    io.out("GREEN", 0, style="GREEN")
+    io.out("BLUE", 0, style="BLUE")
+    io.out("RED", 0, style="RED")
+    io.out("CYAN", 0, style="CYAN")
+    io.out("PURPLE", 0, style="PURPLE")
+    io.out("BOLD", 0, style="BOLD")
+    io.out("UNDERLINE", 0, style="UNDERLINE")
+    io.out("ITALIC", 0, style="ITALIC")
+    io.out("DIM", 0, style="DIM")
+    io.out("This should already be back to regular, but just in case...", 0)
+    io.out("BASIC", 0, style="BASIC")
+    print("io.out() testing complete")
+    time.sleep(10)
+
+    return
 
 
-print("3... 2... 1... GO!\n")
+# Catch all
+if __name__ == "__main__":
+    print(f"Testing in 3...2...1...")
+    main()
+    print(f"\n............Test complete!")
 
-DBG("Debugging not enabled.")
-Console("Enabling Debugging now...")
-Helpers.PrintDebugMessages = True
-DBG("Debugging Enabled.")
-
-print("\nYeah, feeling good about that! :) ")
+# EOF
